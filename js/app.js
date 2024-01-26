e00e07fba0d9c480a90b54475ac25affad179f78
olegonyxov <olegonyxov@gmail.com>
2024-01-26 02:27:55 +0200

    create

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let input1 =prompt("Input some text please")
+let input2 =prompt("Input some text again please")
+let input3 =prompt("and again")
+
+alert(input1+" "+ input2 + " " + input3)
---
+++ js/app1.js
@@ -0,0 +1,17 @@
+let inputNumber = prompt("Input number please");
+inputNumber.split("");
+let rIndex;
+let rIndexList=[];
+while (rIndexList.length != inputNumber.length) { 
+    rIndex = Math.floor(Math.random()*inputNumber.length);
+    if (!rIndexList.includes(rIndex)) {
+    rIndexList.push(rIndex);
+    } 
+}
+let result = [];
+for (let numb of rIndexList){
+    result.push(parseInt(inputNumber[numb]));
+}
+result=result.join(" ");
+alert(result);
+
