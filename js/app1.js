e0f0da01cd7073c68aa55116fca984d40876f17e
olegonyxov <olegonyxov@gmail.com>
2024-02-15 00:37:27 +0200

    8

--- js/app1.js
+++ js/app1.js
@@ -1,5 +1,5 @@
 alert(inputNumber = prompt("Input number please").split("").reverse().join(" "));
 
 // let inputNumber = prompt("Input number please");
-// let inputNumberToList = inputNumber.split("");;
-// result = `${inputNumberToList[4]+" "+inputNumberToList[3]+" "+ inputNumberToList[2]+" "+ inputNumberToList[1]+" "+ inputNumberToList[0]}`
+// let inputNumberToList = inputNumber.split("");
+// result = `${inputNumberToList[4]+" "+inputNumberToList[3]+" "+ inputNumberToList[2]+" "+ inputNumberToList[1]+" "+ inputNumberToList[0]}`;
