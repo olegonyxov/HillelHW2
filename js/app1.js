28394a4ffb4a5e03ab14cf6fd187bb4a24085cba
olegonyxov <olegonyxov@gmail.com>
2024-02-15 00:19:21 +0200

    4

--- js/app1.js
+++ js/app1.js
@@ -1,17 +1,4 @@
 let inputNumber = prompt("Input number please");
-inputNumber.split("");
-let rIndex;
-let rIndexList=[];
-while (rIndexList.length != inputNumber.length) { 
-    rIndex = Math.floor(Math.random()*inputNumber.length);
-    if (!rIndexList.includes(rIndex)) {
-    rIndexList.push(rIndex);
-    } 
-}
-let result = [];
-for (let numb of rIndexList){
-    result.push(parseInt(inputNumber[numb]));
-}
-result=result.join(" ");
+let inputNumberToList = inputNumber.split("");;
+result = `${inputNumberToList[4]+" "+inputNumberToList[3]+" "+ inputNumberToList[2]+" "+ inputNumberToList[1]+" "+ inputNumberToList[0]}`
 alert(result);
-
