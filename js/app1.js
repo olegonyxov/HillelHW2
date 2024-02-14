48620fb9b891e44a7e5e4a562fac354fe55e02c4
olegonyxov <olegonyxov@gmail.com>
2024-02-15 00:41:15 +0200

    10

--- js/app1.js
+++ js/app1.js
@@ -1,5 +1,1 @@
 alert(inputNumber = prompt("Input 5-digit number please").split("").reverse().join(" "));
-
-// let inputNumber = prompt("Input 5-digit number please");
-// let inputNumberToList = inputNumber.split("");
-// result = `${inputNumberToList[4]+" "+inputNumberToList[3]+" "+ inputNumberToList[2]+" "+ inputNumberToList[1]+" "+ inputNumberToList[0]}`;
